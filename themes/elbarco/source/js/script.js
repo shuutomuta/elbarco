cf189731132f23b44fa7ed33e6ba88f332c7fca2
shutomuta <mushu0573@gmail.com>
2019-10-20 00:03:05 +0900

    ヘッダーメニューのフェードインのタイミング変更

--- themes/elbarco/source/js/script.js
+++ themes/elbarco/source/js/script.js
@@ -16,7 +16,7 @@ $(function () {
       var elemPos = jQuery(this).offset().top;
       var scroll = jQuery(window).scrollTop();
       var windowHeight = jQuery(window).height();
-      if (scroll > elemPos - windowHeight +110){
+      if (scroll > elemPos - windowHeight +100){
         jQuery(this).addClass('scrollin');
       }
     });
