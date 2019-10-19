2a76b9a9a891bd2f46fbb079d73f3002b9b4db0c
shutomuta <mushu0573@gmail.com>
2019-10-19 23:49:33 +0900

    ヘッダーメニューのフェードインのタイミング変更

--- themes/elbarco/source/js/script.js
+++ themes/elbarco/source/js/script.js
@@ -1,10 +1,10 @@
 $(function () {
   var headNav = $("header");
 	$(window).on('load scroll', function () {
-		if($(this).scrollTop() > 500 && headNav.hasClass('fixed') == false) {
+		if($(this).scrollTop() > 350 && headNav.hasClass('fixed') == false) {
 			headNav.css({"top": '-100px'});
 			headNav.addClass('fixed');
-			headNav.animate({"top": 0},600);
+			headNav.animate({"top": 0},450);
 		}
 		else if($(this).scrollTop() < 300 && headNav.hasClass('fixed') == true){
 			headNav.removeClass('fixed');
