5d20e0a8cad41f8dd522ed320d8ef12f6c9cd766
shutomuta <mushu0573@gmail.com>
2020-01-16 12:06:33 +0900

    画像遅延読み込み

---
+++ public/js/script.js
@@ -0,0 +1,39 @@
+$(function () {
+  let headNav = $("header");
+	$(window).on('load scroll', function () {
+		if($(this).scrollTop() > 200 && headNav.hasClass('fixed') == false) {
+			headNav.css({"top": '-100px'});
+			headNav.addClass('fixed');
+			headNav.animate({"top": 0},300);
+		}
+		else if($(this).scrollTop() < 100 && headNav.hasClass('fixed') == true){
+			headNav.removeClass('fixed');
+		}
+  });
+
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      let elemPos = $(this).offset().top;
+      let scroll = $(window).scrollTop();
+      let windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight +100){
+        $(this).addClass('scrollin');
+      }
+    });
+  });
+
+  $('#nav__toggle').on('click', function() {
+    $('body').toggleClass('open');
+  });
+});
+function doPost(){
+  $("input[name=submitBtn]").click();
+    if (form.checkValidity()){
+      SendDummy();
+    }else{
+  }
+}
+function SendDummy(){
+  document.getElementById('formWrapper').style.display = 'none';
+  document.getElementById('thxMessage').style.display = 'block';
+}
